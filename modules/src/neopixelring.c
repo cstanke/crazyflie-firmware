58a9ee7c67a856f1e0b6bbd9564fd414c1446da6
cstanke <github001@cstanke.com>
2014-11-02 13:22:55 -0800

    Fine tune color wheel effect to make it look more like police lights. #DEFINE for max pixels. Add thrust effect test. Flip left and right roll in tilt effect.

--- modules/src/neopixelring.c
+++ modules/src/neopixelring.c
@@ -69,41 +69,53 @@
 #define LIMIT(a) ((a>255)?255:(a<0)?0:a)
 #define SIGN(a) ((a>=0)?1:-1)
 
+#define MAX_PIXELS 12
+#define MAIN_TIMER_MS 40
+// #define MAIN_TIMER_MS 1000 // VERY slow spinner
+
 /**************** Black (LEDs OFF) ***************/
 
 static void blackEffect(uint8_t buffer[][3], bool reset)
 {
-  int i;
-  
-  if (reset)
-  {
-    for (i=0; i<12; i++) {
-      buffer[i][0] = 0;
-      buffer[i][1] = 0;
-      buffer[i][2] = 0;
-    }
-  }
-}
-
-static void ledTest(uint8_t buffer[][3], bool reset)
-{
     int i;
-    uint8_t dark_green[3] = {0, 127, 0};
-    uint8_t green[3] = GREEN;
-    uint8_t blue[3] = BLUE;
-    uint8_t red[3] = RED;
-    uint8_t orange[3] = {255, 90, 0};
-    
+  
     if (reset)
     {
-        for (i=0; i<12; i++) {
+        for (i=0; i<MAX_PIXELS; i++) {
             buffer[i][0] = 0;
             buffer[i][1] = 0;
             buffer[i][2] = 0;
         }
     }
-  
+}
+
+/**************** ledTest ***************/
+
+static void ledTest(uint8_t buffer[][3], bool reset)
+{
+    int i;
+    uint8_t dark_green[3] = {0, 20, 0};
+    //uint8_t black[3] = {0, 0, 0};
+    //uint8_t green[3] = GREEN;
+    //uint8_t blue[3] = BLUE;
+    //uint8_t red[3] = RED;
+    //uint8_t orange[3] = {255, 90, 0};
     
+    if (reset)
+    {
+        for (i=0; i<MAX_PIXELS; i++) {
+            buffer[i][0] = 0;
+            buffer[i][1] = 0;
+            buffer[i][2] = 0;
+        }
+    }
+    
+    for (i=0;i<MAX_PIXELS;i++)
+    {
+        COPY_COLOR(buffer[i], dark_green);
+    }
+    
+    /*    
     COPY_COLOR(buffer[11], green);
     COPY_COLOR(buffer[0], dark_green);
     COPY_COLOR(buffer[1], green);
@@ -116,6 +128,7 @@ static void ledTest(uint8_t buffer[][3], bool reset)
     COPY_COLOR(buffer[6], orange);
     COPY_COLOR(buffer[7], red);
     COPY_COLOR(buffer[8], red);
+    */
     
     /*
     COPY_COLOR(buffer[3], test_led[1]);
@@ -123,6 +136,30 @@ static void ledTest(uint8_t buffer[][3], bool reset)
     COPY_COLOR(buffer[9], test_led[1]);
     */
 }
+
+/**************** Thrust ***************/
+
+static void thrustEffect(uint8_t buffer[][3], bool reset)
+{
+    static int thrustid;
+    int i;
+    int thrust_value = 0;
+    
+    if (reset) {
+        //Init
+        thrustid = logGetVarId("stabilizer", "thrust");
+    } else {
+        float thrust = logGetFloat(thrustid);
+        thrust_value = LIMIT(thrust/236);
+        
+        for (i=0; i<MAX_PIXELS; i++) {
+            buffer[i][0] = thrust_value;
+            buffer[i][1] = 0;
+            buffer[i][2] = 0;
+        }
+    }
+}
+
 
 /**************** White spin ***************/
 
@@ -144,7 +181,7 @@ static void whiteSpinEffect(uint8_t buffer[][3], bool reset)
   
   if (reset)
   {
-    for (i=0; i<12; i++) {
+    for (i=0; i<MAX_PIXELS; i++) {
       COPY_COLOR(buffer[i], cw_whiteRing[i]);
     }
   }
@@ -169,9 +206,9 @@ static void whiteSpinEffect(uint8_t buffer[][3], bool reset)
 
 /**************** Color spin ***************/
 
-static const uint8_t colorRing[][3] = {{0,0,24}, {0,0,12}, {0,0,6}, {0,0,3}, {0,0,1}, 
-                                       BLACK, BLACK, 
-                                       {24,0,0}, {12,0,0}, {6,0,0}, {3,0,0}, {1,0,0}
+static const uint8_t colorRing[][3] = {
+                                        BLACK, BLACK, {3,0,0}, {13,0,0}, {50,0,0}, {200,0,0},
+                                        BLACK, BLACK, {0,0,3}, {0,0,13}, {0,0,50}, {0,0,200},
                                       };
 
 static void colorSpinEffect(uint8_t buffer[][3], bool reset)
@@ -181,7 +218,7 @@ static void colorSpinEffect(uint8_t buffer[][3], bool reset)
   
   if (reset)
   {
-    for (i=0; i<12; i++) {
+    for (i=0; i<MAX_PIXELS; i++) {
       COPY_COLOR(buffer[i], colorRing[i]);
     }
   }
@@ -209,6 +246,16 @@ static void tiltEffect(uint8_t buffer[][3], bool reset)
 {
   static int pitchid, rollid, thrust=-1;
   
+  if (reset)
+    {
+        int i;
+        for (i=0; i<MAX_PIXELS; i++) {
+            buffer[i][0] = 0;
+            buffer[i][1] = 0;
+            buffer[i][2] = 0;
+        }
+    }
+  
   if (thrust<0) {
     //Init
     pitchid = logGetVarId("stabilizer", "pitch");
@@ -230,17 +277,17 @@ static void tiltEffect(uint8_t buffer[][3], bool reset)
     buffer[0][0] = LIMIT(led_middle + pitch);
     buffer[1][0] = LIMIT(led_middle + pitch);
     
-    buffer[2][2] = LIMIT(led_middle - roll);
-    buffer[3][2] = LIMIT(led_middle - roll);
-    buffer[4][2] = LIMIT(led_middle - roll);
+    buffer[2][2] = LIMIT(led_middle + roll);
+    buffer[3][2] = LIMIT(led_middle + roll);
+    buffer[4][2] = LIMIT(led_middle + roll);
     
     buffer[5][0] = LIMIT(led_middle - pitch);
     buffer[6][0] = LIMIT(led_middle - pitch);
     buffer[7][0] = LIMIT(led_middle - pitch);
 
-    buffer[8][2] = LIMIT(led_middle + roll);
-    buffer[9][2] = LIMIT(led_middle + roll);
-    buffer[10][2] = LIMIT(led_middle + roll);
+    buffer[8][2] = LIMIT(led_middle - roll);
+    buffer[9][2] = LIMIT(led_middle - roll);
+    buffer[10][2] = LIMIT(led_middle - roll);
   }
 }
 
@@ -249,7 +296,8 @@ static void tiltEffect(uint8_t buffer[][3], bool reset)
 NeopixelRingEffect effectsFct[] = {blackEffect,
                                    ledTest,
                                    whiteSpinEffect, 
-                                   colorSpinEffect, 
+                                   colorSpinEffect,
+                                   thrustEffect,
                                    tiltEffect,
                                   }; //TODO Add more
 
@@ -269,11 +317,11 @@ static uint8_t black[][3] = {BLACK, BLACK, BLACK, BLACK,
 void neopixelringWorker(void * data)
 {
   static int current_effect = 0;
-  static uint8_t buffer[12][3];
+  static uint8_t buffer[MAX_PIXELS][3];
   bool reset = true;
   
   if (!pmIsDischarging() || (effect > neffect)) {
-    ws2812Send(black, 12);
+    ws2812Send(black, MAX_PIXELS);
     return;
   }
   
@@ -285,7 +333,7 @@ void neopixelringWorker(void * data)
   current_effect = effect;
   
   effectsFct[current_effect](buffer, reset);
-  ws2812Send(buffer, 12);
+  ws2812Send(buffer, MAX_PIXELS);
 }
 
 static void neopixelringTimer(xTimerHandle timer)
@@ -299,7 +347,7 @@ void neopixelringInit(void)
   
   neffect = sizeof(effectsFct)/sizeof(effectsFct[0])-1;
   
-  timer = xTimerCreate( (const signed char *)"ringTimer", M2T(40), 
+  timer = xTimerCreate( (const signed char *)"ringTimer", M2T(MAIN_TIMER_MS), 
                                      pdTRUE, NULL, neopixelringTimer );
   xTimerStart(timer, 100);
 }
